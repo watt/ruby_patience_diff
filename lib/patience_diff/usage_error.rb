52fadaa3fe9faaaba9aebbac13ccebb34f64044e
Andrew Watt <andrew.watt@tdameritrade.com>
2012-06-18 17:04:38 -0500

    Fleshed out command-line options.

--- lib/patience_diff/usage_error.rb
+++ lib/patience_diff/usage_error.rb
@@ -1,7 +1,3 @@
 module PatienceDiff
-  class UsageError
-    def print_usage(out=$stderr)
-      out.puts "Usage: #{$0} left-file right-file"
-    end
-  end
+  class UsageError < StandardError ; end
 end
