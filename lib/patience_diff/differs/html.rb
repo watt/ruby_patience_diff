bc74175b5df73827a6ed1c8e0ad3be6fafd87cf1
Andrew Watt <andrewwatt@gmail.com>
2012-07-06 00:37:22 -0500

    More refactoring for formatted templates.

--- lib/patience_diff/differs/html.rb
+++ lib/patience_diff/differs/html.rb
@@ -9,36 +9,38 @@ module PatienceDiff
     # Produces a fancy HTML-formatted unified diff. All your friends will be jealous.
     class Html < Unified
       
-      # Delegate object yielded by the #format method.
-      class Formatter
-        attr_reader :files
-        attr_accessor :title
-        
-        def initialize(differ)
-          @differ = differ
-          @files = []
-          @out = StringIO.new
-          @title = "Diff generated on #{Time.now.strftime('%c')}"
+      class Formatter < PatienceDiff::Differs::Unified::Formatter ; end
+      class HtmlContext
+      end
+      class HunkHelper
+        def render_line(line, code)
+          case code
+          when :equal
+            "<div class='equal diff'> #{escape(line)}</div>"
+          when :delete
+            "<div class='delete diff'>-#{escape(line)}</div>"
+          when :insert
+            "<div class='insert diff'>+#{escape(line)}</div>"
+          end
         end
-        
-        def diff(left, right, left_name=nil, right_name=nil, left_timestamp=nil, right_timestamp=nil)
-          @files << left_name
-          @out.puts @differ.diff(left, right, left_name, right_name, left_timestamp, right_timestamp)
+
+        def escape(raw)
+          CGI::escape_html(raw)
         end
-        
-        def content
-          @out.string
+      end
+      
+      def initialize
+        @erbs = Hash.new do |hash, key|
+          hash[key] = Erubis::Eruby.new(File.read(key))
         end
       end
       
       # Produce a multi-file formatted diff.
       # Yields a delegate object, on which you should call #diff on for each set of files you want in the formatted diff.
       def format
-        raise "Block required" unless block_given?
-        
-        formatter = Formatter.new(self)
-        yield formatter
-        render(formatter)
+        buf = StringIO.new
+        yield Formatter.new(self, buf)
+        template.evaluate(HtmlContext.new(self, buf.string))
       end
             
       private
@@ -49,50 +51,25 @@ module PatienceDiff
           "<div class='right_title diff'>#{escape(right_header)}</div>"
         ]
       end
-        
-      def render_group_header(b, opcodes, last_line_shown)
-        header = super(b, opcodes, last_line_shown)
-        b_start = opcodes.first[3]
-        number_hidden = b_start - last_line_shown - 1
-        if number_hidden > 0
-          [
-            "<div class='collapsed' id='collapsed_#{b_start}'><div>",
-            "<div class='divider' onclick='page.toggleCollapse(#{b_start})'>",
-            "<span class='group_header'>#{'&nbsp;' * header.length}</span>",
-            "Click to collapse #{number_hidden} lines</div>",
-            b[(last_line_shown+1)...b_start].map { |line| render_line(line, :equal) },
-            "</div></div>",
-            "<div class='divider' onclick='page.toggleCollapse(#{b_start})'>",
-            "<span class='group_header'>#{escape(header)}</span>",
-            "<span class='hint'>Click to expand #{number_hidden} lines</span></div>",
-          ]
-        else
-          "<div class='divider'><span class='group_header'>#{escape(header)}</span></div>"
+      
+      def render_hunk(a, b, opcodes, last_hunk_end)
+        hunk_start = opcodes.first[3]
+        helper = HunkHelper.new(b, render_hunk_marker(opcodes), hunk_start, last_hunk_end)
+
+        lines = template("html_header.erb").evaluate(helper)
+        lines << opcodes.collect do |(code, a_start, a_end, b_start, b_end)|
+          if code == :delete 
+            a[a_start..a_end]
+          else
+            b[b_start..b_end]
+          end.map { |line| helper.render_line(line, code) }
         end
+        lines
       end
       
-      def render_line(line, code)
-        case code
-        when :equal
-          "<div class='equal diff'> #{escape(line)}</div>"
-        when :delete
-          "<div class='delete diff'>-#{escape(line)}</div>"
-        when :insert
-          "<div class='insert diff'>+#{escape(line)}</div>"
-        end
-      end
-      
-      def escape(raw)
-        CGI::escape_html(raw)
-      end
       
       def template(filename = "html.erb")
-        File.join(PatienceDiff::TEMPLATE_PATH, filename)
-      end
-      
-      def render(formatter)
-        @erb ||= Erubis::Eruby.new(File.read(template))
-        @erb.evaluate(formatter)
+        @erbs[File.join(PatienceDiff::TEMPLATE_PATH, filename)]
       end
     end
   end
--- lib/patience_diff/differs/unified.rb
+++ lib/patience_diff/differs/unified.rb
@@ -7,13 +7,26 @@ module PatienceDiff
     # Produces plaintext unified diffs.
     class Unified
       
-      # Delegate object returned by the #format method.
+      # Delegate object yielded by the #format method.
       class Formatter
-        def initialize(differ)
+        attr_reader :files
+        attr_accessor :title
+        
+        def initialize(differ, out)
           @differ = differ
+          @files = []
+          @out = out
+          @title = "Diff generated on #{Time.now.strftime('%c')}"
         end
-        def diff(*args)
-          @differ.diff(*args)
+        
+        def diff_files(left_file, right_file)
+          @files << left_name
+          @out.puts @differ.diff_files(left_file, right_file)
+        end
+        
+        def diff(left, right, left_name=nil, right_name=nil, left_timestamp=nil, right_timestamp=nil)
+          @files << left_name
+          @out.puts @differ.diff(left, right, left_name, right_name, left_timestamp, right_timestamp)
         end
       end
       
@@ -23,7 +36,8 @@ module PatienceDiff
       # Options:
       #   * :all_context: Output the entirety of each file. This overrides the sequence matcher's context setting.
       #   * :line_ending: Delimiter to use when joining diff output. Defaults to $RS.
-      #   * :ignore_whitespace: Before comparing lines, strip trailing whitespace, and treat leading whitespace as either present or not. Does not affect output.
+      #   * :ignore_whitespace: Before comparing lines, strip trailing whitespace, and treat leading whitespace 
+      #     as either present or not. Does not affect output.
       # Any additional options (e.g. :context) are passed on to the sequence matcher.
       def initialize(opts = {})
         @all_context = opts.delete(:all_context)
@@ -33,12 +47,26 @@ module PatienceDiff
       end
       
       # Produce a multi-file formatted diff.
-      # Yields a delegate object, on which you should call #diff on for each set of files you want in the formatted diff.
+      # Yields a delegate object, on which you should call #diff or #diff_files for each set of files you want in 
+      # the formatted diff.
       def format
-        yield Formatter.new(self)
+        buf = StringIO.new
+        yield Formatter.new(self, buf)
+        buf.string
       end
       
-      # Diff 2 files.
+      # Generates a unified diff from the contents of the files at the paths specified.
+      def diff_files(left_file, right_file)
+        (left_data, left_timestamp), (right_data, right_timestamp) = [left_file, right_file].map do |filename|
+          # Read in binary encoding, so that we can diff any encoding and split() won't complain
+          File.open(filename, :external_encoding => Encoding::BINARY) do |file|
+            [file.read.split($RS), file.mtime]
+          end
+        end
+        diff(left_data, right_data, left_file, right_file, left_timestamp, right_timestamp)
+      end
+      
+      # Generate a unified diff of the data specified. The left and right values should be strings, or any other indexable, sortable data.
       # File names and timestamps do not affect the diff algorithm, but are used in the header text.
       def diff(left, right, left_name=nil, right_name=nil, left_timestamp=nil, right_timestamp=nil)
         if @ignore_whitespace
@@ -50,17 +78,16 @@ module PatienceDiff
         end
         
         if @all_context
-          groups = [@matcher.diff_opcodes(a, b)]
+          hunks = [@matcher.diff_opcodes(a, b)]
         else
-          groups = @matcher.grouped_opcodes(a, b)
+          hunks = @matcher.grouped_opcodes(a, b)
         end
         
         lines = render_header(left_name, right_name, left_timestamp, right_timestamp)
         last_line_shown = -1
-        groups.each do |group|
-          lines << render_group_header(b, group, last_line_shown)
-          lines << render_diff_group(a, b, group)
-          last_line_shown = group.last[4]
+        hunks.each do |hunk|
+          lines << render_hunk(a, b, hunk, last_line_shown)
+          last_line_shown = hunk.last[4]
         end
         lines.flatten.compact.join(@line_ending)
       end
@@ -77,7 +104,7 @@ module PatienceDiff
         ]
       end
       
-      def render_group_header(b, opcodes, last_line_shown)
+      def render_hunk_marker(opcodes)
         a_start = opcodes.first[1] + 1
         a_end = opcodes.last[2] + 2
         b_start = opcodes.first[3] + 1
@@ -86,14 +113,17 @@ module PatienceDiff
         "@@ -%d,%d +%d,%d @@" % [a_start, a_end-a_start, b_start, b_end-b_start]
       end
       
-      def render_diff_group(a, b, opcodes)
-        opcodes.collect do |(code, a_start, a_end, b_start, b_end)|
+      def render_hunk(a, b, opcodes, last_line_shown)
+        lines = []
+        lines << render_hunk_marker(opcodes)
+        lines << opcodes.collect do |(code, a_start, a_end, b_start, b_end)|
           if code == :delete 
             a[a_start..a_end]
           else
             b[b_start..b_end]
           end.map { |line| render_line(line, code) }
         end
+        lines
       end
       
       def render_line(line, code)
